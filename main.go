664dd41c70ce0874af4a3c16162a12120493aa09
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-05-18 14:22:54 +0530

    added all structs

--- main.go
+++ main.go
@@ -11,7 +11,14 @@ import (
 // It is used to store the data of the blockchain and the books
 
 // Block is used to store the data of the block
+// for Block we're not going to have any json tags beacuse we are not going to send this data to the user
+// we don't have any apis for our project, we are just going to use this json data for sending to postman for testing but block is created by itself when we hit new in route and it will store the data of bookcheckout struct in it.
 type Block struct {
+	Position  int
+	Data      BookCheckout
+	TimeStamp string
+	Hash      string
+	PrevHash  string
 }
 
 // BookCheckout is used to store the data of the book checkout
@@ -37,6 +44,8 @@ type Blockchain struct {
 	blocks []*Block
 }
 
+// why we are not using any databse : Blockchain is itself a database. It's store the transactional information.
+// Block is created and store in this blockchain variable
 var blockchain *Blockchain //global variable, we are going to use this to store our blockchain
 
 // we are creating three functions for our blockchain which are getBlockchain, writeBlock and newBook
@@ -65,3 +74,10 @@ func main() {
 	log.Fatal(http.ListenAndServe(":3000", r))
 
 }
+
+// go mod tidy - to install all the dependencies
+// go get -u github.com/gorilla/mux - to install gorilla mux
+// go run main.go - to run the project
+// go build - to build the project
+// ./blockchain - to run the build project
+// go mod init github.com/yourusername/blockchain - to create a module
